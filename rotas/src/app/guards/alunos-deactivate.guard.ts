1113b4820fe36e10c5a3464f3a833f4bc6d22aad
danielgomes42 <damsilva757@gmail.com>
2022-03-09 22:49:06 -0300

    interface and guard

--- rotas/src/app/guards/alunos-deactivate.guard.ts
+++ rotas/src/app/guards/alunos-deactivate.guard.ts
@@ -5,17 +5,17 @@ import { Injectable } from '@angular/core';
 import { AlunoFormComponent } from './../alunos/aluno-form/aluno-form.component';
 
 @Injectable()
-export class AlunosDeactivateGuard implements CanDeactivate<IFormCanDeactivate> {
+export class AlunosDeactivateGuard implements CanDeactivate<IFormCanDeactivate> { // coloca a interface aqui (iformcandeactivate)
                 
         canDeactivate(
-            component: IFormCanDeactivate,
+            component: IFormCanDeactivate, // ele manda para a interface esse metodo 
             route: ActivatedRouteSnapshot,
             state: RouterStateSnapshot
         ): Observable<boolean>|Promise<boolean>|boolean {
 
-            console.log('guarda de desativação');
+            console.log('guarda de desativação');// todas as rotas que tivem a marcação desse serviço em alunos.rounting vai mostra guarda de desativação
 
-            //return component.podeMudarRota ? component.podeMudarRota() : true;
+            //return component.podeMudarRota ? component.podeMudarRota() : true;  // esse é metoda para mostra a msg ou não quando mundo de rota 
 
             return component.podeDesativar ? component.podeDesativar() : true;
     }
